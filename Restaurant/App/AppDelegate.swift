4cc6c07c2f759659184fad4a66857eff2795899c
Aleksander Shigin <a.sh.10031987@gmail.com>
2019-09-21 13:10:12 +0300

    clear appDelegate

---
+++ Restaurant/App/AppDelegate.swift
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.swift
+//  Restaurant
+//
+//  Created by Alexander on 29/07/2019.
+//  Copyright © 2019 Alexander Shigin. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+}
+
---
+++ Restaurant/ViewControllers/MenuTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  MenuTableViewController.swift
+//  Restaurant
+//
+//  Created by Alexander on 30/07/2019.
+//  Copyright © 2019 Alexander Shigin. All rights reserved.
+//
+
+import UIKit
+
+class MenuTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+}
